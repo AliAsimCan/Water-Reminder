1c14ac3d94373302cc5c84ac1705740b67987a60
zeynepgunduz <138715784+zeynepgunduz@users.noreply.github.com>
2023-07-06 16:25:01 +0300

    Add files via upload

---
+++ WaterReminder.py
@@ -0,0 +1,19 @@
+import time
+from plyer import notification
+
+#Uyarilar kac saatte bir yapılsın
+while True  :
+    pSureAralik=input("Uyarilar kac saat aralıkla yapılsın?")
+    if (pSureAralik.isnumeric())==True:
+        pSureAralik=int(pSureAralik)*60
+        break
+
+
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "Su İçme Vakti",
+            message ="Sağlıklı Yaşam İçin Su İçin!.",
+            timeout= 10
+            )
+        time.sleep(pSureAralik)
